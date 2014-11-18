2460c611ca8a54d10b8a67c290860adf95a67b77
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-18 06:36:57 +0000

    openssh 6.7p1
    New formula. Storing this here until Homebrew-dupes is at a position
    where it can ship this there, presumably when keychain-support is
    available for this release.

---
+++ openssh.rb
@@ -0,0 +1,48 @@
+require "formula"
+
+class Openssh < Formula
+  homepage "http://www.openssh.com/"
+  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz"
+  mirror "http://ftp.eu.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz"
+  version "6.7p1"
+  sha256 "b2f8394eae858dabbdef7dac10b99aec00c95462753e80342e530bbb6f725507"
+
+  option "with-ldns", "build with ldns"
+
+  depends_on "openssl"
+
+  if build.with? "ldns"
+    depends_on "pkg-config" => :build
+    depends_on "ldns"
+  end
+
+  patch do
+    url "https://gist.githubusercontent.com/sigkate/fca7ee9fe1cdbe77ba03/raw/6894261e7838d81c76ef4b329e77e80d5ad25afc/patch-openssl-darwin-sandbox.diff"
+    sha1 "332a1831bad9f2ae0f507f7ea0aecc093829b1c4"
+  end
+
+  # Patch for SSH tunnelling issues caused by launchd changes on Yosemite
+  patch do
+    url "https://trac.macports.org/export/128279/trunk/dports/net/openssh/files/launchd.patch"
+    sha1 "e35731b6d0e999fb1d58362cda2574c0d1efed78"
+  end
+
+  def install
+    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
+
+    args = %W[
+      --with-libedit
+      --with-pam
+      --with-kerberos5
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}/ssh
+      --with-ssl-dir=#{Formula["openssl"].opt_prefix}
+    ]
+
+    args << "--with-ldns" if build.with? "ldns"
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+end
