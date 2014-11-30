1f1b6c33b9b205651d586c72c2f039eb5f2365bd
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-30 01:40:20 +0000

    openssh: added libressl option
    Want to build the latest portable OpenSSH with the latest portable
    LibreSSL? Now you can. The default remains OpenSSL.

--- Formula/bopenssh.rb
+++ Formula/bopenssh.rb
@@ -6,15 +6,12 @@ class Bopenssh < Formula
   mirror "http://ftp.eu.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz"
   version "6.7p1"
   sha256 "b2f8394eae858dabbdef7dac10b99aec00c95462753e80342e530bbb6f725507"
-
-  bottle do
-    root_url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Homebrew/Tap_Bottles"
-    sha1 "efc24d7d013e63597adaea1ac38811f9d5595af1" => :yosemite
-  end
+  revision 1
 
   option "with-ldns", "build with ldns"
 
-  depends_on "openssl"
+  depends_on "libressl" => :optional
+  depends_on "openssl" => :optional
 
   if build.with? "ldns"
     depends_on "pkg-config" => :build
@@ -41,10 +38,14 @@ class Bopenssh < Formula
       --with-kerberos5
       --prefix=#{prefix}
       --sysconfdir=#{etc}/ssh
-      --with-ssl-dir=#{Formula["openssl"].opt_prefix}
     ]
 
     args << "--with-ldns" if build.with? "ldns"
+    if build.with? "libressl"
+      args << "--with-ssl-dir=#{Formula["libressl"].opt_prefix}"
+    else
+      args << "--with-ssl-dir=#{Formula["openssl"].opt_prefix}"
+    end
 
     system "./configure", *args
     system "make"
