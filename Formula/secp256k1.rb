82eaad5408126cde62172293371561a67610de74
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-10 04:15:08 +0000

    secp256k1 0.0.0.6
    Commit bump to sync with upstream.

--- Formula/secp256k1.rb
+++ Formula/secp256k1.rb
@@ -1,7 +1,7 @@
 class Secp256k1  < Formula
   homepage "https://github.com/bitcoin/secp256k1"
-  url "https://github.com/bitcoin/secp256k1.git", :revision => "67cbdf044003a1cbc98550b0bea069e8678b8b87"
-  version "0.0.0.5" # Fake version number to make updates easier.
+  url "https://github.com/bitcoin/secp256k1.git", :revision => "2d4cd53087227c55451ba7d2de24044379e62f3c"
+  version "0.0.0.6" # Fake version number to make updates easier.
   head "https://github.com/bitcoin/secp256k1.git"
 
   depends_on "automake" => :build
