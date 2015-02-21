645d25c0f5017318b690f590d5dec47f71a00f01
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-21 05:35:14 +0000

    bopenssh: drop keychain-support
    The patch has some potentially major issues, and it’s not an area I
    particularly want to be screwing around with the source code of. In
    hindsight, I should have stuck with my gut and kept the patch removed.

--- Formula/bopenssh.rb
+++ Formula/bopenssh.rb
@@ -4,23 +4,14 @@ class Bopenssh < Formula
   mirror "http://ftp.eu.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz"
   version "6.7p1"
   sha256 "b2f8394eae858dabbdef7dac10b99aec00c95462753e80342e530bbb6f725507"
-  revision 4
+  revision 5
 
   option "with-ldns", "build with ldns"
-  option "with-keychain-support", "Add native OS X Keychain and Launch Daemon support to ssh-agent"
 
-  depends_on "autoconf" => :build if build.with? "keychain-support"
-  depends_on "pkg-config" => :build if build.with? "ldns"
+  depends_on "pkg-config" => :build
   depends_on "libressl" => :recommended
   depends_on "openssl" => :optional
   depends_on "ldns" => :optional
-
-  if build.with? "keychain-support"
-    patch do
-      url "https://trac.macports.org/export/131258/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch"
-      sha1 "ceafaee8814d4ce7477d2c75cddc7e3b209f038a"
-    end
-  end
 
   patch do
     url "https://gist.githubusercontent.com/sigkate/fca7ee9fe1cdbe77ba03/raw/6894261e7838d81c76ef4b329e77e80d5ad25afc/patch-openssl-darwin-sandbox.diff"
@@ -34,13 +25,6 @@ class Bopenssh < Formula
   end
 
   def install
-    system "autoreconf", "-i" if build.with? "keychain-support"
-
-    if build.with? "keychain-support"
-      ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
-      ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
-    end
-
     ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
 
     args = %W[
@@ -62,39 +46,5 @@ class Bopenssh < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
-  end
-
-  def caveats
-    if build.with? "keychain-support" then <<-EOS.undent
-        NOTE: replacing system daemons is unsupported. Proceed at your own risk.
-
-        For complete functionality, please modify:
-          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-
-        and change ProgramArguments from
-          /usr/bin/ssh-agent
-        to
-          #{HOMEBREW_PREFIX}/bin/ssh-agent
-
-        You will need to restart or issue the following commands
-        for the changes to take effect:
-
-          launchctl unload /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-          launchctl load /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-
-        Finally, add  these lines somewhere to your ~/.bash_profile:
-          eval $(ssh-agent)
-
-          function cleanup {
-            echo "Killing SSH-Agent"
-            kill -9 $SSH_AGENT_PID
-          }
-
-          trap cleanup EXIT
-
-        After that, you can start storing private key passwords in
-        your OS X Keychain.
-      EOS
-    end
   end
 end
