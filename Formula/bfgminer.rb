128c16ed9da1f7675c5077c0a0947a9d4f11d49f
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-13 13:11:13 +0000

    bfgminer: fix compile
    Apologies, the `uthash` dep should have been handled this way, not the
    way it was before. This will fix various issues.

--- Formula/bfgminer.rb
+++ Formula/bfgminer.rb
@@ -8,7 +8,6 @@ class Bfgminer < Formula
   option "with-cpumining", "Build with cpumining support"
 
   depends_on "pkg-config" => :build
-  depends_on "uthash" => :build
   depends_on "curl" => :recommended
   depends_on "hidapi"
   depends_on "jansson"
@@ -16,7 +15,16 @@ class Bfgminer < Formula
   depends_on "libevent"
   depends_on "libusb"
 
+  resource "uthash" do
+    url "https://github.com/troydhanson/uthash/archive/v1.9.9.tar.gz"
+    sha256 "f2f10115f4de03b98d8c2e860e241d165aa15019bd634ee0e3debe58341bbf0c"
+  end
+
   def install
+    resource("uthash").stage do
+      (buildpath/"uthash/include").install "src/utarray.h", "src/uthash.h", "src/utlist.h", "src/utstring.h"
+    end
+
     args = %W[
       --prefix=#{prefix}
       --disable-debug
@@ -28,6 +36,8 @@ class Bfgminer < Formula
     args << "--enable-scrypt" if build.with? "scrypt"
     args << "--enable-keccak" if build.with? "keccak"
     args << "--enable-cpumining" if build.with? "cpumining"
+
+    ENV.append_to_cflags "-I#{buildpath}/uthash/include"
 
     system "./configure", *args
     system "make", "install"
