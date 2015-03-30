fa4ab32469f324d095a3b7fdcdf3b53af1ca6b9b
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-30 19:06:47 +0100

    boringssl 0.0.0.17
    Sync with latest upstream commit.

--- Formula/boringssl.rb
+++ Formula/boringssl.rb
@@ -1,7 +1,7 @@
 class Boringssl < Formula
   homepage "https://boringssl.googlesource.com"
-  url "https://boringssl.googlesource.com/boringssl.git", :revision => "90fa69aaae7434344f22e93287f43fa85a4dd249"
-  version "0.0.0.16" # Fake version so we can update the formula regularly & easily.
+  url "https://boringssl.googlesource.com/boringssl.git", :revision => "4616bb1e6e0387de15eabb95575974d3ba467307"
+  version "0.0.0.17" # Fake version so we can update the formula regularly & easily.
 
   keg_only :provided_by_osx, <<-EOS.undent
     "Apple provides a deprecated OpenSSL, which conflicts with this.
