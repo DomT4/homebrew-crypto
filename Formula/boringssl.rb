8220f368345fe69c9a76c42fcdbf8d9fc17649d1
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-27 16:47:27 +0100

    boringssl 0.0.0.22
    Fake version bump = Sync with upstream's latest commit.

--- Formula/boringssl.rb
+++ Formula/boringssl.rb
@@ -1,7 +1,7 @@
 class Boringssl < Formula
   homepage "https://boringssl.googlesource.com"
-  url "https://boringssl.googlesource.com/boringssl.git", :revision => "83f182c6c5f6d1318329c2fe645f74d832e83701"
-  version "0.0.0.21" # Fake version so we can update the formula regularly & easily.
+  url "https://boringssl.googlesource.com/boringssl.git", :revision => "41846c74f1668e95cb709e74db03f67a48d03c8c"
+  version "0.0.0.22" # Fake version so we can update the formula regularly & easily.
   head "https://boringssl.googlesource.com/boringssl.git"
 
   keg_only :provided_by_osx, <<-EOS.undent
