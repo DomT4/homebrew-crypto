416e7a62977035b7c75fddb09149a502670b30dd
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-05 04:15:43 +0100

    mosmans-openssl 1.0.2_chacha_m8
    Sync with latest upstream commit.

--- Formula/mosmans-openssl.rb
+++ Formula/mosmans-openssl.rb
@@ -1,8 +1,8 @@
 class MosmansOpenssl < Formula
   homepage "https://www.onwebsecurity.com/cryptography/openssl"
   url "https://github.com/PeterMosmans/openssl.git",
-      :branch => "1.0.2-chacha", :revision => "33acc886134df1134560c3172aab681add242246"
-  version "1.0.2_chacha_m7"
+      :branch => "1.0.2-chacha", :revision => "5f22a45f32e3d90d8410269c243f8738ecb92ecb"
+  version "1.0.2_chacha_m8"
 
   option :universal
   option "without-check", "Skip build-time tests (not recommended)"
