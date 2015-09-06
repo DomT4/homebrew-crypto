a420ba3da0fa52f39435e655aac8014e1b44f5ff
Dominyk Tiller <dominyktiller@gmail.com>
2015-09-07 00:45:42 +0100

    bitcoind: add plist

--- Formula/bitcoind.rb
+++ Formula/bitcoind.rb
@@ -1,5 +1,5 @@
 class Bitcoind < Formula
-  desc "An innovative decentralized, peer-to-peer payment network"
+  desc "Innovative decentralized, peer-to-peer payment network"
   homepage "https://bitcoin.org/en/"
   url "https://github.com/bitcoin/bitcoin/archive/v0.11.0.tar.gz"
   sha256 "2bcd61a4c288e5cc5d7fbe724606c610a20037332b06f7a9e99c1153eef73aef"
@@ -41,6 +41,29 @@ class Bitcoind < Formula
     system "make", "check"
     system "make", "install"
     bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  plist_options :manual => "bitcoind -daemon"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/bitcoind</string>
+          <string>-daemon</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
   end
 
   def caveats; <<-EOS.undent
