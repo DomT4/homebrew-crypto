d5ecdee654810e5a4210b18f6ac92d8d7353acc1
Dominyk Tiller <dominyktiller@gmail.com>
2015-09-07 01:05:25 +0100

    secp256k1 0.0.0.22
    Closes #35.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Formula/secp256k1.rb
+++ Formula/secp256k1.rb
@@ -1,8 +1,8 @@
 class Secp256k1 < Formula
   desc "Bitcoin’s experimental curves library"
   homepage "https://github.com/bitcoin/secp256k1"
-  url "https://github.com/bitcoin/secp256k1.git", :revision => "5eb435694eb632367658995c6ad0592e3f4fe768"
-  version "0.0.0.21" # Fake version number to make updates easier.
+  url "https://github.com/bitcoin/secp256k1.git", :revision => "85e3a2cc087993973a2195849c652005b0be7ddd"
+  version "0.0.0.22" # Fake version number to make updates easier.
   head "https://github.com/bitcoin/secp256k1.git"
 
   depends_on "automake" => :build
