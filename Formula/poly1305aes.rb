25adabc68dcc4b35a3d306a58f458485511fe2a2
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-15 20:14:09 +0000

    poly1305aes: use secure urls

--- Formula/poly1305aes.rb
+++ Formula/poly1305aes.rb
@@ -1,10 +1,12 @@
 class Poly1305aes < Formula
   desc "State-of-the-art message-authentication code"
-  homepage "http://cr.yp.to/mac.html"
-  url "http://cr.yp.to/mac/poly1305aes-20050218.tar.gz"
+  homepage "https://cr.yp.to/mac.html"
+  url "https://cr.yp.to/mac/poly1305aes-20050218.tar.gz"
   sha256 "1ff45c186f85a8a2533d88ae10b6ea7510be93d592c22d751db21023ef0a7133"
 
-  fails_with :clang
+  fails_with :clang do
+    cause "fatal: all tests failed!"
+  end
 
   def install
     ENV.append_to_cflags "-O2"
