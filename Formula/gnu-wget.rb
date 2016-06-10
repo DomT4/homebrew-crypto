b95ec9ce19c1048cd5580c391c142711c9d062f8
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-10 13:12:26 +0100

    gnu-wget 1.18

--- Formula/gnu-wget.rb
+++ Formula/gnu-wget.rb
@@ -3,9 +3,9 @@
 class GnuWget < Formula
   desc "Internet file retriever built against LibreSSL"
   homepage "https://www.gnu.org/software/wget/"
-  url "http://ftpmirror.gnu.org/wget/wget-1.17.1.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/wget/wget-1.17.1.tar.xz"
-  sha256 "fe559b61eb9cc01635ac6206a14e02cb51591838c35fa83c7a4aacae0bdd97c9"
+  url "http://ftpmirror.gnu.org/wget/wget-1.18.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/wget/wget-1.18.tar.xz"
+  sha256 "b5b55b75726c04c06fe253daec9329a6f1a3c0c1878e3ea76ebfebc139ea9cc1"
 
   head do
     url "git://git.savannah.gnu.org/wget.git"
@@ -26,6 +26,10 @@ class GnuWget < Formula
   depends_on "gpgme" => :optional
 
   def install
+    # Fixes undefined symbols _iconv, _iconv_close, _iconv_open
+    # Reported 10 Jun 2016: https://savannah.gnu.org/bugs/index.php?48193
+    ENV.append "LDFLAGS", "-liconv"
+
     args = %W[
       --prefix=#{prefix}
       --sysconfdir=#{etc}
@@ -48,10 +52,17 @@ class GnuWget < Formula
     mv "#{man1}/wget.1", "#{man1}/lwget.1" if build.without? "default-names"
   end
 
-  def caveats; <<-EOS.undent
-    The binary is prepended with a 'l' so this can be used
-    alongside Homebrew/Homebrew's wget without conflict.
-    EOS
+  def caveats
+    s = ""
+
+    if build.without? "default-names"
+      s += <<-EOS.undent
+        The binary is prepended with a 'l' so this can be used
+        alongside Homebrew/Homebrew's wget without conflict.
+      EOS
+    end
+
+    s
   end
 
   test do
