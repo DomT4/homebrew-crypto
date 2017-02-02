40a3f9efd71e17a58a6f7a285cceba60a89544b1
Dominyk Tiller <dominyktiller@gmail.com>
2017-02-02 19:32:02 +0000

    bearssl 0.3

--- Formula/bearssl.rb
+++ Formula/bearssl.rb
@@ -1,8 +1,8 @@
 class Bearssl < Formula
   desc "Implementation of the SSL/TLS protocol written in C"
   homepage "https://bearssl.org/index.html"
-  url "https://bearssl.org/bearssl-0.2.tar.gz"
-  sha256 "8d7640b4112e7cf677a4a5f22a997cf89c4a18e51bc4843f6f16b7db7fe93f73"
+  url "https://bearssl.org/bearssl-0.3.tar.gz"
+  sha256 "2797ccb08f41e1aeabb625c8682f4b2e61f1827695d4ea10a57c5b62175a3b22"
   head "https://www.bearssl.org/git/BearSSL", :using => :git
 
   depends_on "doxygen" => [:build, :optional]
@@ -17,9 +17,12 @@ class Bearssl < Formula
     end
 
     # No "make install".
-    bin.install "brssl"
+    cd "build" do
+      bin.install "brssl"
+      lib.install "libbearssl.a"
+      lib.install "libbearssl.so" => "libbearssl.dylib"
+    end
     (include/"bearssl").install Dir["inc/*.h"]
-    lib.install "libbearssl.a"
   end
 
   def caveats; <<-EOS.undent
