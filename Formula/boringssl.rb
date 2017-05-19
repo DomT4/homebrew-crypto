067acb97d804e4f9e408ae8e89a2a0389f53fa13
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-19 20:56:55 +0100

    boringssl 0.0.0.68

--- Formula/boringssl.rb
+++ Formula/boringssl.rb
@@ -2,8 +2,8 @@ class Boringssl < Formula
   desc "Google fork of OpenSSL"
   homepage "https://boringssl.googlesource.com"
   url "https://boringssl.googlesource.com/boringssl.git",
-      :revision => "30bcb3bd283ac992e7b1e8848fd450ad560b4122"
-  version "0.0.0.67" # Fake version so we can update the formula regularly.
+      :revision => "429e85b516ec967458da3281ff6fd05a0b5170c8"
+  version "0.0.0.68" # Fake version so we can update the formula regularly.
   head "https://boringssl.googlesource.com/boringssl.git"
 
   keg_only <<-EOS.undent
