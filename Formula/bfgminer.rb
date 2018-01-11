c39d7d9a04443c47b0c2c9fe294dcee8f7ec3920
Dominyk Tiller <dominyktiller@gmail.com>
2018-01-11 03:26:13 +0000

    bfgminer 5.5.0

--- Formula/bfgminer.rb
+++ Formula/bfgminer.rb
@@ -1,14 +1,13 @@
 class Bfgminer < Formula
   desc "Modular ASIC/FPGA miner written in C"
   homepage "https://github.com/luke-jr/bfgminer"
-  url "http://luke.dashjr.org/programs/bitcoin/files/bfgminer/5.4.2/bfgminer-5.4.2.zip"
-  sha256 "9be4b7374ee90751cf221ff3cf8b697a5f3034b11aedb6ffbae0911e3560412c"
+  url "http://bfgminer.org/files/5.5.0/bfgminer-5.5.0.txz"
+  sha256 "ac254da9a40db375cb25cacdd2f84f95ffd7f442e31d2b9a7f357a48d32cc681"
 
-  option "without-scrypt", "Build without Scrypt support"
-  option "with-keccak", "Build with Keccak support"
   option "with-cpumining", "Build with cpumining support"
 
   depends_on "pkg-config" => :build
+  depends_on "yasm" => :build
   depends_on "curl" => :recommended
   depends_on "hidapi"
   depends_on "jansson"
@@ -17,27 +16,26 @@ class Bfgminer < Formula
   depends_on "libusb"
 
   resource "uthash" do
-    url "https://github.com/troydhanson/uthash/archive/v1.9.9.tar.gz"
-    sha256 "f2f10115f4de03b98d8c2e860e241d165aa15019bd634ee0e3debe58341bbf0c"
+    url "https://github.com/troydhanson/uthash/archive/v2.0.2.tar.gz"
+    sha256 "34a31d51dd7a839819cecd6f46049b4ffe031d7f3147d9a042f5504fdb1348d1"
   end
 
   def install
     resource("uthash").stage do
-      (buildpath/"uthash/include").install "src/utarray.h", "src/uthash.h", "src/utlist.h", "src/utstring.h"
+      (buildpath/"uthash/include").install Dir["src/*.h"]
     end
-
     ENV.append_to_cflags "-I#{buildpath}/uthash/include"
+
     args = %W[
       --prefix=#{prefix}
       --disable-debug
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-keccak
       --enable-opencl
+      --enable-scrypt
       --without-system-libbase58
     ]
-
-    args << "--enable-scrypt" if build.with? "scrypt"
-    args << "--enable-keccak" if build.with? "keccak"
     args << "--enable-cpumining" if build.with? "cpumining"
 
     system "./configure", *args
