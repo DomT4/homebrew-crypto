eb48424a21beaaf870e62e6c44b42c65a8a9425d
Dominyk Tiller <dominyktiller@gmail.com>
2018-02-14 19:27:33 +0000

    gnu-wget: fix wget conflict

--- Formula/gnu-wget.rb
+++ Formula/gnu-wget.rb
@@ -4,6 +4,7 @@ class GnuWget < Formula
   url "https://ftp.gnu.org/gnu/wget/wget-1.19.4.tar.gz"
   mirror "https://ftpmirror.gnu.org/wget/wget-1.19.4.tar.gz"
   sha256 "93fb96b0f48a20ff5be0d9d9d3c4a986b469cb853131f9d5fe4cc9cecbc8b5b5"
+  revision 1
 
   head do
     url "https://git.savannah.gnu.org/git/wget.git"
@@ -45,6 +46,7 @@ class GnuWget < Formula
     if build.without? "default-names"
       mv info/"wget.info", info/"lwget.info"
       mv man1/"wget.1", man1/"lwget.1"
+      rm_rf share/"locale" # Not worth renaming every single file here.
     end
   end
 
