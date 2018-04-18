c8ed3e8b351f47521a8c3d1e44371fbd58aed849
Dominyk Tiller <dominyktiller@gmail.com>
2018-04-18 04:33:26 +0100

    secp256k1 0.0.0.46

--- Formula/secp256k1.rb
+++ Formula/secp256k1.rb
@@ -2,8 +2,8 @@ class Secp256k1 < Formula
   desc "Bitcoin experimental curves library"
   homepage "https://github.com/bitcoin/secp256k1"
   url "https://github.com/bitcoin/secp256k1.git",
-      :revision => "fb9271dcf043f81435bdcdb81156938ae74e3407"
-  version "0.0.0.45" # Fake version number to make updates easier.
+      :revision => "452d8e4d2a2f9f1b5be6b02e18f1ba102e5ca0b4"
+  version "0.0.0.46" # Fake version number to make updates easier.
   head "https://github.com/bitcoin/secp256k1.git"
 
   depends_on "automake" => :build
