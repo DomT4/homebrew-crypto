9c9a5ee853db58c445845f930f6a746745ad40a3
Dominyk Tiller <dominyktiller@gmail.com>
2018-09-05 18:46:28 +0100

    git-max 2.18.0 (new formula)

---
+++ Formula/git-max.rb
@@ -0,0 +1,160 @@
+class GitMax < Formula
+  desc "Distributed revision control system"
+  homepage "https://git-scm.com"
+  url "https://www.kernel.org/pub/software/scm/git/git-2.18.0.tar.xz"
+  sha256 "8b40be383a603147ae29337136c00d1c634bdfdc169a30924a024596a7e30e92"
+  head "https://github.com/git/git.git", :shallow => false
+
+  depends_on "go" => :build
+  depends_on "domt4/crypto/curl-max"
+  depends_on "openssl@1.1"
+  # So bindings can be found & used by Homebrew's Perl.
+  depends_on "perl" => :recommended
+  depends_on "pcre2"
+  depends_on "gettext" => :recommended
+  depends_on "subversion" => :optional
+
+  conflicts_with "git",
+    :because => "git-max is a feature-heavy version of the git formula"
+
+  resource "html" do
+    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.18.0.tar.xz"
+    sha256 "5b70d9b1f86aad464ae172040eb87da83058c00581388af6f60b050214912efc"
+  end
+
+  resource "man" do
+    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.18.0.tar.xz"
+    sha256 "9ae3be95e63379597164c79c9018e84b7ef6964da923e5689dbc307693266096"
+  end
+
+  def install
+    # If these things are installed, tell Git build system not to use them
+    ENV["NO_FINK"] = "1"
+    ENV["NO_DARWIN_PORTS"] = "1"
+    # This is the default now when not using CommonCrypto but set it regardless
+    # in case we ever move back to CommonCrypto here in future.
+    ENV["DC_SHA1"] = "1"
+    ENV["V"] = "1" # build verbosely
+    ENV["NO_R_TO_GCC_LINKER"] = "1" # pass arguments to LD correctly
+    ENV["PYTHON_PATH"] = which("python")
+    ENV["PERL_PATH"] = which("perl")
+
+    perl_version = Utils.popen_read("perl --version")[/v(\d+\.\d+)(?:\.\d+)?/, 1]
+    # If building with a non-system Perl search everywhere declared in @INC.
+    perl_inc = Utils.popen_read("perl -e 'print join\":\",@INC'").sub(":.", "")
+
+    if build.with? "subversion"
+      ENV["PERLLIB_EXTRA"] = %W[
+        #{Formula["subversion"].opt_lib}/perl5/site_perl
+      ].join(":")
+    elsif build.with? "perl"
+      ENV["PERLLIB_EXTRA"] = perl_inc
+    elsif MacOS.version >= :mavericks
+      ENV["PERLLIB_EXTRA"] = %W[
+        #{MacOS.active_developer_dir}
+        /Library/Developer/CommandLineTools
+        /Applications/Xcode.app/Contents/Developer
+      ].uniq.map do |p|
+        "#{p}/Library/Perl/#{perl_version}/darwin-thread-multi-2level"
+      end.join(":")
+    end
+
+    unless quiet_system ENV["PERL_PATH"], "-e", "use ExtUtils::MakeMaker"
+      ENV["NO_PERL_MAKEMAKER"] = "1"
+    end
+
+    ENV["NO_GETTEXT"] = "1" if build.without? "gettext"
+    ENV["USE_LIBPCRE2"] = "1"
+    ENV["LIBPCREDIR"] = Formula["pcre2"].opt_prefix
+
+    args = %W[
+      prefix=#{prefix}
+      sysconfdir=#{etc}
+      CC=#{ENV.cc}
+      CFLAGS=#{ENV.cflags}
+      LDFLAGS=#{ENV.ldflags}
+      NO_APPLE_COMMON_CRYPTO=1
+      OPENSSLDIR=#{Formula["openssl@1.1"].opt_prefix}
+    ]
+
+    system "make", "install", *args
+
+    git_core = libexec/"git-core"
+
+    # Install the macOS keychain credential helper
+    cd "contrib/credential/osxkeychain" do
+      system "make", "CC=#{ENV.cc}",
+                     "CFLAGS=#{ENV.cflags}",
+                     "LDFLAGS=#{ENV.ldflags}"
+      git_core.install "git-credential-osxkeychain"
+      system "make", "clean"
+    end
+
+    # Generate diff-highlight perl script executable
+    cd "contrib/diff-highlight" do
+      system "make"
+    end
+
+    # Install the netrc credential helper
+    cd "contrib/credential/netrc" do
+      system "make", "test"
+      git_core.install "git-credential-netrc"
+    end
+
+    # Install git-subtree
+    cd "contrib/subtree" do
+      system "make", "CC=#{ENV.cc}",
+                     "CFLAGS=#{ENV.cflags}",
+                     "LDFLAGS=#{ENV.ldflags}"
+      git_core.install "git-subtree"
+    end
+
+    cd "contrib/persistent-https" do
+      system "make"
+      git_core.install "git-remote-persistent-http",
+                       "git-remote-persistent-https",
+                       "git-remote-persistent-https--proxy"
+    end
+
+    # install the completion script first because it is inside "contrib"
+    bash_completion.install "contrib/completion/git-completion.bash"
+    bash_completion.install "contrib/completion/git-prompt.sh"
+
+    zsh_completion.install "contrib/completion/git-completion.zsh" => "_git"
+    cp "#{bash_completion}/git-completion.bash", zsh_completion
+
+    elisp.install Dir["contrib/emacs/*.el"]
+    (share/"git-core").install "contrib"
+
+    # We could build the manpages ourselves, but the build process depends
+    # on many other packages, and is somewhat crazy, this way is easier.
+    man.install resource("man")
+    (share/"doc/git-doc").install resource("html")
+
+    # Make html docs world-readable
+    chmod 0644, Dir["#{share}/doc/git-doc/**/*.{html,txt}"]
+    chmod 0755, Dir["#{share}/doc/git-doc/{RelNotes,howto,technical}"]
+
+    # This is only created when building against system Perl, but it isn't
+    # purged by Homebrew's post-install cleaner because that doesn't check
+    # "Library" directories. It is however pointless to keep around as it
+    # only contains the perllocal.pod installation file.
+    rm_rf prefix/"Library/Perl"
+
+    # Set the macOS keychain credential helper by default
+    # (as Apple's CLT's git also does this).
+    (buildpath/"gitconfig").write <<~EOS
+      [credential]
+      \thelper = osxkeychain
+    EOS
+    etc.install "gitconfig"
+  end
+
+  test do
+    system bin/"git", "init"
+    %w[haunted house].each { |f| touch testpath/f }
+    system bin/"git", "add", "haunted", "house"
+    system bin/"git", "commit", "-a", "-m", "Initial Commit"
+    assert_equal "haunted\nhouse", shell_output("#{bin}/git ls-files").strip
+  end
+end
