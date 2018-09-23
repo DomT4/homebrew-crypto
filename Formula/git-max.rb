d6ed3c901a7582f8d9f3017ec15e36b34769a6a6
Dominyk Tiller <dominyktiller@gmail.com>
2018-09-23 01:06:46 +0100

    git-max: set SOURCE_DATE_EPOCH

--- Formula/git-max.rb
+++ Formula/git-max.rb
@@ -41,6 +41,7 @@ class GitMax < Formula
     ENV["DC_SHA1"] = "1"
     ENV["V"] = "1" # build verbosely
     ENV["NO_INSTALL_HARDLINKS"] = "1" # https://github.com/Homebrew/brew/issues/4921
+    ENV["SOURCE_DATE_EPOCH"] = "1"
     ENV["NO_R_TO_GCC_LINKER"] = "1" # pass arguments to LD correctly
     ENV["PYTHON_PATH"] = which("python")
     ENV["PERL_PATH"] = which("perl")
