78d5d9e3d24d81b8b58ed573bf4a4f22613102ad
Dominyk Tiller <dominyktiller@gmail.com>
2018-10-31 18:54:51 +0000

    curl-max 7.62.0

--- Formula/curl-max.rb
+++ Formula/curl-max.rb
@@ -1,10 +1,9 @@
 class CurlMax < Formula
   desc "Feature-maximised version of cURL, using OpenSSL 1.1"
   homepage "https://curl.haxx.se/"
-  url "https://curl.haxx.se/download/curl-7.61.1.tar.bz2"
-  mirror "http://curl.mirror.anstey.ca/curl-7.61.1.tar.bz2"
-  sha256 "a308377dbc9a16b2e994abd55455e5f9edca4e31666f8f8fcfe7a1a4aea419b9"
-  revision 1
+  url "https://curl.haxx.se/download/curl-7.62.0.tar.bz2"
+  mirror "http://curl.mirror.anstey.ca/curl-7.62.0.tar.bz2"
+  sha256 "7802c54076500be500b171fde786258579d60547a3a35b8c5a23d8c88e8f9620"
 
   bottle do
     root_url "https://dl.bintray.com/domt4/crypto-bottles"
