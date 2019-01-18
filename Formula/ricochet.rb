ec43069ee77c526f12ffaf6fb8f17b390576c7c9
Dominyk Tiller <dominyktiller@gmail.com>
2019-01-18 03:54:15 +0000

    ricochet: update tor to 0.3.5.7

--- Formula/ricochet.rb
+++ Formula/ricochet.rb
@@ -3,7 +3,7 @@ class Ricochet < Formula
   homepage "https://ricochet.im"
   url "https://ricochet.im/releases/1.1.4/ricochet-1.1.4-src.tar.bz2"
   sha256 "f5f32caa3480def1de5c93010c6bf5f5789ddcba34bf09fc0feab67696d0c374"
-  revision 18
+  revision 19
   head "https://github.com/ricochet-im/ricochet.git"
 
   depends_on "pkg-config" => :build
@@ -13,9 +13,9 @@ class Ricochet < Formula
   depends_on "libevent" # For Tor
 
   resource "tor" do
-    url "https://www.torproject.org/dist/tor-0.3.4.9.tar.gz"
-    mirror "https://tor.eff.org/dist/tor-0.3.4.9.tar.gz"
-    sha256 "1a171081f02b9a6ff9e28c0898defb7670e5bbb3bdbcaddfcf4e4304aedd164a"
+    url "https://www.torproject.org/dist/tor-0.3.5.7.tar.gz"
+    mirror "https://tor.eff.org/dist/tor-0.3.5.7.tar.gz"
+    sha256 "1b0887fc21ac535befea7243c5d5f1e31394d7458d64b30807a3e98cca0d839e"
   end
 
   def install
