12d1c8e45a19ee475ef34bd52bfabd8b576447f4
Dominyk Tiller <dominyktiller@gmail.com>
2021-11-07 17:52:32 +0000

    secp256k1 0.0.0.63

--- Formula/secp256k1.rb
+++ Formula/secp256k1.rb
@@ -2,8 +2,8 @@ class Secp256k1 < Formula
   desc "Optimized C library for EC operations on curve secp256k1"
   homepage "https://github.com/bitcoin-core/secp256k1"
   url "https://github.com/bitcoin-core/secp256k1.git",
-      revision: "d7ec49a6893751f068275cc8ddf4993ef7f31756"
-  version "0.0.0.62" # Fake version number to make updates easier.
+      revision: "8fa41201bde8844f79198401c60ec57fa84517e3"
+  version "0.0.0.63" # Fake version number to make updates easier.
   head "https://github.com/bitcoin-core/secp256k1.git"
 
   option "without-enable-module-recovery", "Disable ECDSA pubkey recovery module"
